2461ee7ed12fed9d18272a6ee7e511475d356049
Andres Rios Bolivar <andresriosbolivar27@gmail.com>
2023-08-16 22:24:15 -0500

    Add Country Entity

---
+++ Sales/Sales.Backend/Data/DataContext.cs
@@ -0,0 +1,22 @@
+﻿
+
+namespace Sales.Backend.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using Sales.Shared.Entities;
+
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Country> MyProperty { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasIndex(c => c.Name).IsUnique();
+        }
+    }
+}
---
+++ Sales/Sales.Shared/Entities/Country.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace Sales.Shared.Entities
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class Country
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Pais")]
+        [MaxLength(100, ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string Name { get; set; } = null;
+    }
+}
